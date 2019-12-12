7006872cc0e336258ff37b4a9ed3219872b91798
Amy Bemister <amy.bemister@gmail.com>
2019-12-12 01:47:59 +0000

    Add instance variables for counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class Admin::DashboardController < ApplicationController
   def show
+    @products = Product.order(id: :desc).all
+    @product_count = Product.count
+    @category_count = Category.count
   end
 end
