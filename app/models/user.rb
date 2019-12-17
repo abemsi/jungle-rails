d8487b1230da7aa04936120d324d11d421e28256
Amy Bemister <amy.bemister@gmail.com>
2019-12-17 17:52:11 +0000

    Add validations, define authenticate_with_credentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,17 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  def authenticate_with_credentials(email, password)
+    new_email = email.strip.downcase
+    user = User.find_by_email(new_email)
+    if user && user.authenticate(password)
+      user
+    else
+      nil
+    end
+  end
+
+  validates :name, :email, :password_digest, presence: true
+  validates :email, uniqueness: true, case_sensitive: false
+  validates :password, length: { minimum: 5 }
 end
